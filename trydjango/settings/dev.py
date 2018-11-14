13b5146c2832c5a1ff8b2739e4330ec93d1fbd5e
Katie O Donnell <katiemodonnell88@gmail.com>
2018-11-14 11:37:49 +0000

    inital commit

---
+++ trydjango/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SYSTEM_EMAIL = 'admin@mysitename.com'
